b18deba3f100f6d237b06dab9f86974b262d2d75
jipark96 <wlgus996@naver.com>
2023-04-18 21:37:54 +0900

    메인 페이지 - header(nav) 컴포넌트 #2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header_wrap">
+        <div className="header_content">
+          <img
+            src="https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Ficon-menu.png&w=17&q=75"
+            className="header_img1"
+          ></img>
+          <div className="header_p1">wanted</div>
+        </div>
+        <span className="header_ul">
+          <div className="header_li">채용</div>
+          <div className="header_li">이벤트</div>
+          <div className="header_li">직군별 연봉</div>
+          <div className="header_li">이력서</div>
+          <div className="header_li">프리랜서</div>
+          <div className="header_li">AI 합격예측</div>
+        </span>
+        <span className="header_span">
+          <div className="header_li2">
+            <img
+              src={process.env.PUBLIC_URL + "/search.png"}
+              className="header_img2"
+            />
+          </div>
+          <div className="header_li3">회원가입/로그인</div>
+          <div className="header_li4"></div>
+          <div className="header_li5">기업 서비스</div>
+        </span>
+      </div>
     </div>
   );
 }
