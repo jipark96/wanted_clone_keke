a6b57dc03730527cd98af88556cbd3031bea9ea8
jipark96 <wlgus996@naver.com>
2023-04-19 18:13:00 +0900

    채용페이지 하는중

--- src/components/layout/main/employmentInformation.js
+++ src/components/layout/main/employmentInformation.js
@@ -1,13 +1,15 @@
+import { useState } from "react";
+
 function EmploymentInformation () {
 
-    // let [information, setInformation] = useState["채용공고","내 프로필","매치업","직군별 연봉"]
+    let [information, setInformation] = useState(["채용공고","내 프로필","매치업","직군별 연봉"])
     return (
       <>    <div className="employmentInfo">
           <div className="employmentInfo_div1">
             <h2 className="employmentInfo_h2">채용 정보를 찾고 계셨나요?</h2>
           </div>
           <div className="employmentInfo_wrap">
-          {/* {
+          {
               information.map((a,i) => {
                 return (
                   <div className="employmentInfo_item">
@@ -15,8 +17,8 @@ function EmploymentInformation () {
                   </div>
                 )
               })
-            } */}
-            <div className="employmentInfo_item">
+            }
+            {/* <div className="employmentInfo_item">
               <div className="employmentInfo_div2">채용공고</div>
             </div>
             <div className="employmentInfo_item">
@@ -27,7 +29,7 @@ function EmploymentInformation () {
             </div>
             <div className="employmentInfo_item">
               <div className="employmentInfo_div2">직군별 연봉</div>
-            </div>
+            </div> */}
           </div>
         </div>
           <hr/>
---
+++ src/data.js
@@ -0,0 +1,136 @@
+let companyData = [
+    {
+        job : "웹디자이너",
+        name : "무신사",
+        speed : "응답률 매우 높음",
+        region : "서울·한국",
+        compensation : "채용보상금 1,000,000원"
+    },
+    {
+        job : "채용 담당자",
+        name : "우아한청년들",
+        speed : "응답률 매우 높음",
+        region : "서울·한국",
+        compensation : "채용보상금 1,000,000원"
+    },
+    {
+        job : "프론트엔드",
+        name : "원티드",
+        speed : "응답률 매우 높음",
+        region : "서울·한국",
+        compensation : "채용보상금 1,000,000원"
+    },
+    {
+        job : "백엔드",
+        name : "삼성",
+        speed : "응답률 매우 높음",
+        region : "서울·한국",
+        compensation : "채용보상금 1,000,000원"
+    },
+    {
+        job : "웹디자이너",
+        name : "무신사",
+        speed : "응답률 매우 높음",
+        region : "서울·한국",
+        compensation : "채용보상금 1,000,000원"
+    },
+    {
+        job : "웹디자이너",
+        name : "무신사",
+        speed : "응답률 매우 높음",
+        region : "서울·한국",
+        compensation : "채용보상금 1,000,000원"
+    },
+    {
+        job : "웹디자이너",
+        name : "무신사",
+        speed : "응답률 매우 높음",
+        region : "서울·한국",
+        compensation : "채용보상금 1,000,000원"
+    },
+    {
+        job : "웹디자이너",
+        name : "무신사",
+        speed : "응답률 매우 높음",
+        region : "서울·한국",
+        compensation : "채용보상금 1,000,000원"
+    },
+    {
+        job : "웹디자이너",
+        name : "무신사",
+        speed : "응답률 매우 높음",
+        region : "서울·한국",
+        compensation : "채용보상금 1,000,000원"
+    },
+    {
+        job : "웹디자이너",
+        name : "무신사",
+        speed : "응답률 매우 높음",
+        region : "서울·한국",
+        compensation : "채용보상금 1,000,000원"
+    },
+    {
+        job : "웹디자이너",
+        name : "무신사",
+        speed : "응답률 매우 높음",
+        region : "서울·한국",
+        compensation : "채용보상금 1,000,000원"
+    },
+    {
+        job : "웹디자이너",
+        name : "무신사",
+        speed : "응답률 매우 높음",
+        region : "서울·한국",
+        compensation : "채용보상금 1,000,000원"
+    },
+    {
+        job : "웹디자이너",
+        name : "무신사",
+        speed : "응답률 매우 높음",
+        region : "서울·한국",
+        compensation : "채용보상금 1,000,000원"
+    },
+    {
+        job : "웹디자이너",
+        name : "무신사",
+        speed : "응답률 매우 높음",
+        region : "서울·한국",
+        compensation : "채용보상금 1,000,000원"
+    },
+    {
+        job : "웹디자이너",
+        name : "무신사",
+        speed : "응답률 매우 높음",
+        region : "서울·한국",
+        compensation : "채용보상금 1,000,000원"
+    },
+    {
+        job : "웹디자이너",
+        name : "무신사",
+        speed : "응답률 매우 높음",
+        region : "서울·한국",
+        compensation : "채용보상금 1,000,000원"
+    },
+    {
+        job : "웹디자이너",
+        name : "무신사",
+        speed : "응답률 매우 높음",
+        region : "서울·한국",
+        compensation : "채용보상금 1,000,000원"
+    },
+    {
+        job : "웹디자이너",
+        name : "무신사",
+        speed : "응답률 매우 높음",
+        region : "서울·한국",
+        compensation : "채용보상금 1,000,000원"
+    },
+    {
+        job : "웹디자이너",
+        name : "무신사",
+        speed : "응답률 매우 높음",
+        region : "서울·한국",
+        compensation : "채용보상금 1,000,000원"
+    }
+];
+export default companyData;
